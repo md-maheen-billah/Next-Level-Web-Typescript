bfbda01f17660838c697c020d92362ad556cfb88
md-maheen-billah <md.maheen.billah@icloud.com>
2024-11-02 03:06:01 +0100

    Constraints using key of

---
+++ module2/src/2.7.ts
@@ -0,0 +1,31 @@
+{//////////////////////////////////////////////
+// Generic Constraint with keyof operator
+
+type Vehicle = {
+    bike: string;
+    car: string;
+    ship:string
+}
+
+type Owner = "bike" | "car" | "ship"; //manually
+
+type Owner2 = keyof Vehicle  //same as above but done using keyof
+
+const person1: Owner = "car"
+
+const  getPropertyValue = <X,Y extends keyof X>(obj:X,key:Y) =>{
+return obj[key];
+}
+
+
+const user = {
+    name: 'Mr. X',
+    age: 27,
+    address: 'dhk'
+}
+
+const result1 = getPropertyValue(user,'address')
+
+
+
+}//////////////////////////////////////////////
