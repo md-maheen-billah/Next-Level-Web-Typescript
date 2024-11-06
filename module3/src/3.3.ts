881b6e30a4aa11278cdc5397097d1c7a2cdad447
md-maheen-billah <md.maheen.billah@icloud.com>
2024-11-06 19:44:42 +0100

    type guard using typeof and in

---
+++ module3/src/3.3.ts
@@ -0,0 +1,52 @@
+{//////////////////////////////////////////////
+//oop- Type guard using typeof & in
+
+// type of guard
+
+type Alphanumeric = string|number;
+const add = (param1: Alphanumeric, param2: Alphanumeric):Alphanumeric=>{
+
+    if(typeof param1 === 'number'&& typeof param2 === 'number') {
+        return param1+param2
+    } else {
+        return param1.toString() + param2.toString();
+    }
+    
+}
+
+const result1 = add(2,'3');
+console.log(result1);
+
+// in guard
+
+type NormalUser = {
+    name: string;
+}
+
+type AdminUser = {
+    name: string;
+    role: 'admin'
+}
+
+const getUser = (user: NormalUser|AdminUser)=>{
+    if('role'in user){
+        console.log(`My name is ${user.name} and my role is ${user.role}`);
+    }
+    else{
+        console.log(`My name is ${user.name} and i have no roles`);
+    }
+}
+
+const normalUser: NormalUser = {
+    name: 'Mr. Normal',
+}
+
+const adminUser: AdminUser = {
+    name: 'Mr. Admin',
+    role: 'admin',
+}
+
+getUser(adminUser);
+getUser(normalUser);
+
+}//////////////////////////////////////////////
